374b90be044e7152057702d7446c43cc6e07502c
LucLor06 <LukeLorenzBA@gmail.com>
2024-11-15 17:15:41 -0500

    Add kwarg validation and show kwarg to hs_dump tag

--- templatetags/hyperscript.py
+++ templatetags/hyperscript.py
@@ -6,7 +6,22 @@ from django.template.context import RequestContext
 register = template.Library()
 
 @register.simple_tag()
-def hs_dump(data, name='data'):
+def hs_dump(data, name='data', **kwargs):
+    ACCEPTED_KWARGS = {'show': bool}
+    
+    for key, value in kwargs.items():
+        if key not in ACCEPTED_KWARGS:
+            raise TypeError(f'Unexpected keyword argument: {key}. Accepted arguments: {', '.join([f'{kwarg}: {type.__name__}' for kwarg, type in ACCEPTED_KWARGS.items()])}.')
+        
+        expected_type = ACCEPTED_KWARGS[key]
+        if not isinstance(value, expected_type):
+            raise TypeError(f'Invalid type for keyword argument {key}: expected {expected_type}, got {str(type(value))}')
+
     data = json.dumps(data)
-    hyperscript = f"<div _='init set global {name} to {data} end'></div>"
+    hyperscript = f'init set global {name} to {data}'
+
+    if not kwargs.get('show', False):
+        hyperscript = f'{hyperscript} then remove me'
+        
+    hyperscript = f"<div _='{hyperscript} end'></div>"
     return mark_safe(hyperscript)
