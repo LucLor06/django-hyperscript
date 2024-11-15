0f781894a864742df90d1c5fb8643e99c2fe539e
LucLor06 <LukeLorenzBA@gmail.com>
2024-11-15 17:39:42 -0500

    Add translate kwarg to hs_dump tag

--- templatetags/hyperscript.py
+++ templatetags/hyperscript.py
@@ -5,9 +5,19 @@ from django.template.context import RequestContext
 
 register = template.Library()
 
+def _snake_case_to_camel_case(data: str):
+    words = data.split('_')
+    return f'{words[0]}{''.join([word.capitalize() for word in words[1:]])}'
+
+def _dict_to_camel_case(data: dict):
+    if isinstance(data, dict):
+        return {_snake_case_to_camel_case(key): _dict_to_camel_case(value) for key, value in data.items()}
+    else:
+        return data
+
 @register.simple_tag()
 def hs_dump(data, name='data', **kwargs):
-    ACCEPTED_KWARGS = {'show': bool}
+    ACCEPTED_KWARGS = {'show': bool, 'translate': bool}
     
     for key, value in kwargs.items():
         if key not in ACCEPTED_KWARGS:
@@ -17,8 +27,12 @@ def hs_dump(data, name='data', **kwargs):
         if not isinstance(value, expected_type):
             raise TypeError(f'Invalid type for keyword argument {key}: expected {expected_type}, got {str(type(value))}')
 
+    if kwargs.get('translate', True):
+        data = _dict_to_camel_case(data)
+
     data = json.dumps(data)
     hyperscript = f'init set global {name} to {data}'
+
 
     if not kwargs.get('show', False):
         hyperscript = f'{hyperscript} then remove me'
