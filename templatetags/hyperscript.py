b80a6fa0c01e5a9be8d1796ff189f7de7094824b
LucLor06 <LukeLorenzBA@gmail.com>
2024-11-15 16:52:28 -0500

    Change hs_dump tag to have explicitly set name

--- templatetags/hyperscript.py
+++ templatetags/hyperscript.py
@@ -5,15 +5,8 @@ from django.template.context import RequestContext
 
 register = template.Library()
 
-@register.simple_tag(takes_context=True)
-def hs_dump(context, data):
-    context = context.dicts[-2] if isinstance(context, RequestContext) else context
-    data_name = 'data'
-        
-    for key, value in context.items():
-        if data == value:
-            data_name = key
-            
+@register.simple_tag()
+def hs_dump(data, name='data'):
     data = json.dumps(data)
-    hyperscript = f"<div _='init set global {data_name} to {data} then remove me end'></div>"
+    hyperscript = f"<div _='init set global {name} to {data} end'></div>"
     return mark_safe(hyperscript)
