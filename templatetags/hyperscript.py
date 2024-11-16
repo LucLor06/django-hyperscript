6dc9d9c6c56509c38147d52254a5d2e6f420d5ad
LucLor06 <LukeLorenzBA@gmail.com>
2024-11-16 01:28:12 -0500

    Move hs_dump logic to external function. Add hs_expand tag.

--- templatetags/hyperscript.py
+++ templatetags/hyperscript.py
@@ -1,7 +1,6 @@
 from django import template
 from django.utils.safestring import mark_safe
 import json
-from django.template.context import RequestContext
 
 register = template.Library()
 
@@ -15,27 +14,39 @@ def _dict_to_camel_case(data: dict):
     else:
         return data
 
-@register.simple_tag()
-def hs_dump(data, name='data', **kwargs):
-    ACCEPTED_KWARGS = {'show': bool, 'translate': bool}
-    
+def _construct_hyperscript(data, accepted_kwargs: dict, name=None, **kwargs):
     for key, value in kwargs.items():
-        if key not in ACCEPTED_KWARGS:
-            raise TypeError(f'Unexpected keyword argument: {key}. Accepted arguments: {', '.join([f'{kwarg}: {type.__name__}' for kwarg, type in ACCEPTED_KWARGS.items()])}.')
+        if key not in accepted_kwargs:
+            raise TypeError(f'Unexpected keyword argument: {key}. Accepted arguments: {', '.join([f'{kwarg}: {type.__name__}' for kwarg, type in accepted_kwargs.items()])}.')
         
-        expected_type = ACCEPTED_KWARGS[key]
+        expected_type = accepted_kwargs[key]
         if not isinstance(value, expected_type):
-            raise TypeError(f'Invalid type for keyword argument {key}: expected {expected_type}, got {str(type(value))}')
-
+            raise TypeError(f'Invalid type for keyword argument {key}: expected {expected_type}, got {type(value).__name__}')
+    
     if kwargs.get('translate', True):
         data = _dict_to_camel_case(data)
 
-    data = json.dumps(data)
-    hyperscript = f'init set global {name} to {data}'
+    if kwargs.get('expand', False):
+        if not isinstance(data, dict):
+            raise TypeError(f'Invalid type for mapping: expected dict, got {type(data).__name__}')
+        assignment = ' '.join([f'set global {key} to {json.dumps(value)}' for key, value in data.items()])
+    else:
+        assignment = f'set global {name} to {json.dumps(data)}'
 
-
+    hyperscript = f'init {assignment}'
     if not kwargs.get('show', False):
         hyperscript = f'{hyperscript} then remove me'
-        
-    hyperscript = f"<div _='{hyperscript} end'></div>"
+    hyperscript = f'{hyperscript} end'
+    hyperscript = f"<div _='{hyperscript}'></div>"
     return mark_safe(hyperscript)
+
+@register.simple_tag()
+def hs_dump(data, name: str, **kwargs):
+    ACCEPTED_KWARGS = {'show': bool, 'translate': bool}
+    return _construct_hyperscript(data, ACCEPTED_KWARGS, name, **kwargs)
+
+@register.simple_tag()
+def hs_expand(data, **kwargs):
+    kwargs['expand'] = True
+    ACCEPTED_KWARGS = {'show': bool, 'translate': bool, 'expand': bool}
+    return _construct_hyperscript(data, ACCEPTED_KWARGS, **kwargs)
